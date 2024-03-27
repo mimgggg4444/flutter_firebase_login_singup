384c0d4d26e1e9b33aee8578a63acd6384ededd5
e <minnki19@gmail.com>
2024-03-27 23:58:16 +0900

    change sign up image done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_login_singup/login_page.dart';
+import 'package:flutter_firebase_login_singup/signup_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:LoginPage()
+      home:SignUpPage()
     );
   }
 }
--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -24,164 +24,165 @@ class SignUpPage extends StatelessWidget {
               ),
 
             ),
-            Container(
-              margin: const EdgeInsets.only(left: 20, right: 20),
-              width: w,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Hello",
-                    style: TextStyle(
-                      fontSize: 70,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    "아이디와 비밀번호를 입력해주세요",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.grey[500],
-                    ),
-                  ),
-                  SizedBox(height: 50,),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 10,
-                              spreadRadius: 7,
-                              offset: Offset(1,1),
-                              color: Colors.grey.withOpacity(0.2)
-                          )
-                        ]
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30),
-                              borderSide: BorderSide(
-                                  color: Colors.white,
-                                  width: 1.0
-                              )
-                          ),
 
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30),
-                              borderSide: BorderSide(
-                                  color: Colors.white,
-                                  width: 1.0
-                              )
-                          ),
-
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30)
-                          )
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 10,
-                              spreadRadius: 7,
-                              offset: Offset(1,1),
-                              color: Colors.grey.withOpacity(0.2)
-                          )
-                        ]
-                    ),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30),
-                              borderSide: BorderSide(
-                                  color: Colors.white,
-                                  width: 1.0
-                              )
-                          ),
-
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30),
-                              borderSide: BorderSide(
-                                  color: Colors.white,
-                                  width: 1.0
-                              )
-                          ),
-
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(30)
-                          )
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20,),
-                  Row(
-                    children: [
-                      Expanded(child: Container(),),
-                      Text(
-                        "아이디와 비밀번호를 입력해주세요",
-                        style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.grey[500],
-                        ),
-                      ),
-                    ],
-                  ),
-
-
-                ],
-              ),
-            ),
-            SizedBox(height: 70,),
-            Container(
-              width: w*.5,
-              height: h*.08,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  image: DecorationImage(
-                      image: AssetImage(
-                          "img/loginbtn.png"
-                      ),
-                      fit: BoxFit.cover
-                  )
-              ),
-              child: Center(
-                child: Text(
-                  "로그인",
-                  style: TextStyle(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-
-                  ),
-                ),
-              ),
-
-            ),
-            SizedBox(height: w*.07),
-            RichText(text: TextSpan(
-                text: "아이디가 없으신가요?",
-                style: TextStyle(
-                  color: Colors.grey[500],
-                  fontSize: 20,
-                ),
-                children: [
-                  TextSpan(
-                      text: " 회원가입",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        // fontWeight: FontWeight.bold,
-                      )
-                  )
-                ]
-            ))
+            // Container(
+            //   margin: const EdgeInsets.only(left: 20, right: 20),
+            //   width: w,
+            //   child: Column(
+            //     crossAxisAlignment: CrossAxisAlignment.start,
+            //     children: [
+            //       Text(
+            //         "Hello",
+            //         style: TextStyle(
+            //           fontSize: 70,
+            //           fontWeight: FontWeight.bold,
+            //         ),
+            //       ),
+            //       Text(
+            //         "아이디와 비밀번호를 입력해주세요",
+            //         style: TextStyle(
+            //           fontSize: 20,
+            //           color: Colors.grey[500],
+            //         ),
+            //       ),
+            //       SizedBox(height: 50,),
+            //       Container(
+            //         decoration: BoxDecoration(
+            //             color: Colors.white,
+            //             borderRadius: BorderRadius.circular(30),
+            //             boxShadow: [
+            //               BoxShadow(
+            //                   blurRadius: 10,
+            //                   spreadRadius: 7,
+            //                   offset: Offset(1,1),
+            //                   color: Colors.grey.withOpacity(0.2)
+            //               )
+            //             ]
+            //         ),
+            //         child: TextField(
+            //           decoration: InputDecoration(
+            //               focusedBorder: OutlineInputBorder(
+            //                   borderRadius: BorderRadius.circular(30),
+            //                   borderSide: BorderSide(
+            //                       color: Colors.white,
+            //                       width: 1.0
+            //                   )
+            //               ),
+            //
+            //               enabledBorder: OutlineInputBorder(
+            //                   borderRadius: BorderRadius.circular(30),
+            //                   borderSide: BorderSide(
+            //                       color: Colors.white,
+            //                       width: 1.0
+            //                   )
+            //               ),
+            //
+            //               border: OutlineInputBorder(
+            //                   borderRadius: BorderRadius.circular(30)
+            //               )
+            //           ),
+            //         ),
+            //       ),
+            //       SizedBox(height: 20,),
+            //       Container(
+            //         decoration: BoxDecoration(
+            //             color: Colors.white,
+            //             borderRadius: BorderRadius.circular(30),
+            //             boxShadow: [
+            //               BoxShadow(
+            //                   blurRadius: 10,
+            //                   spreadRadius: 7,
+            //                   offset: Offset(1,1),
+            //                   color: Colors.grey.withOpacity(0.2)
+            //               )
+            //             ]
+            //         ),
+            //         child: TextField(
+            //           decoration: InputDecoration(
+            //               focusedBorder: OutlineInputBorder(
+            //                   borderRadius: BorderRadius.circular(30),
+            //                   borderSide: BorderSide(
+            //                       color: Colors.white,
+            //                       width: 1.0
+            //                   )
+            //               ),
+            //
+            //               enabledBorder: OutlineInputBorder(
+            //                   borderRadius: BorderRadius.circular(30),
+            //                   borderSide: BorderSide(
+            //                       color: Colors.white,
+            //                       width: 1.0
+            //                   )
+            //               ),
+            //
+            //               border: OutlineInputBorder(
+            //                   borderRadius: BorderRadius.circular(30)
+            //               )
+            //           ),
+            //         ),
+            //       ),
+            //       SizedBox(height: 20,),
+            //       Row(
+            //         children: [
+            //           Expanded(child: Container(),),
+            //           Text(
+            //             "아이디와 비밀번호를 입력해주세요",
+            //             style: TextStyle(
+            //               fontSize: 20,
+            //               color: Colors.grey[500],
+            //             ),
+            //           ),
+            //         ],
+            //       ),
+            //
+            //
+            //     ],
+            //   ),
+            // ),
+            // SizedBox(height: 70,),
+            // Container(
+            //   width: w*.5,
+            //   height: h*.08,
+            //   decoration: BoxDecoration(
+            //       borderRadius: BorderRadius.circular(30),
+            //       image: DecorationImage(
+            //           image: AssetImage(
+            //               "img/loginbtn.png"
+            //           ),
+            //           fit: BoxFit.cover
+            //       )
+            //   ),
+            //   child: Center(
+            //     child: Text(
+            //       "로그인",
+            //       style: TextStyle(
+            //         fontSize: 36,
+            //         fontWeight: FontWeight.bold,
+            //         color: Colors.white,
+            //
+            //       ),
+            //     ),
+            //   ),
+            //
+            // ),
+            // SizedBox(height: w*.07),
+            // RichText(text: TextSpan(
+            //     text: "아이디가 없으신가요?",
+            //     style: TextStyle(
+            //       color: Colors.grey[500],
+            //       fontSize: 20,
+            //     ),
+            //     children: [
+            //       TextSpan(
+            //           text: " 회원가입",
+            //           style: TextStyle(
+            //             color: Colors.black,
+            //             fontSize: 20,
+            //             // fontWeight: FontWeight.bold,
+            //           )
+            //       )
+            //     ]
+            // ))
           ],
         )
     );
