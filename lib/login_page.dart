967d9e94540b405e3b79bb74000f5b44e91fdf95
e <minnki19@gmail.com>
2024-03-27 23:03:13 +0900

    page setup fin

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -5,8 +5,53 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
 
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: Column(
+        children: [
+          Container(
+            width: w,
+            height: h*0.3,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                  "img/loginimg.png"
+                ),
+                fit: BoxFit.cover
+              )
+            ),
+
+          ),
+          Container(
+            margin: const EdgeInsets.only(left: 20, right: 20),
+            width: w,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hello",
+                  style: TextStyle(
+                    fontSize: 70,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "아이디와 비밀번호를 입력해주세요",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.grey[500],
+                  ),
+                )
+
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
